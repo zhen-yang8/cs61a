45e78bcf9af1f0a8465c4e12209954fec0b44f5c
Zhen Yang <393661355@qq.com>
2021-06-12 13:21:53 +0800

    add lab01

---
+++ lab01/lab01.py
@@ -0,0 +1,57 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    mul = 1
+    for i in range(k):
+        mul = mul * (n - i)
+    return mul
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    res = 0
+    for c in str(y):
+        res += int(c)
+    return res
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    import re
+    if re.search('88', str(n)) is not None:
+        return True
+    return False
+
